d4f014c117f68bcb80165ebc312a2be6fc5c7548
Shripad Ige <shripadige925@gmail.com>
2025-03-14 20:16:38 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -36,7 +36,13 @@ const Body = () => {
 
   async function getRestaurantList() {
 
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6599188&lng=75.9063906&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6599188&lng=75.9063906&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",{
+        method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
+    });
 
     const json = await data.json();
 
